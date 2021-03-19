502d9d35eed9a8f72e5721e8850e5c744b480884
unknown <lomasian@hotmail.ca>
2021-03-19 17:28:59 -0400

    inital commit

---
+++ JS/game.js
@@ -0,0 +1,157 @@
+var config = {
+  type: Phaser.AUTO,
+  width: 800,
+  height: 600,
+  backgroundColor: '#efefef',
+  parent: 'phaser-example',
+  physics: {
+      default: 'matter',
+      matter: {
+          debug: true,
+          gravity: {
+            x: 0,
+            y: 0
+        }
+      }
+  },
+  scene: {
+    preload: preload, create: create, update: update
+  }
+};
+
+var objects = []
+var poly2
+var compoundBody
+var game = new Phaser.Game(config);
+
+function preload () {
+  this.load.image('block', '../assets/images/rectangle.png');
+}
+
+function create () {
+  // this.matter.world.setBounds().disableGravity();
+
+  var arrow = '40 0 40 20 100 20 100 80 40 80 40 100 0 50';
+  var chevron = '100 0 75 50 100 100 25 100 0 50 25 0';
+  var star = '50 0 63 38 100 38 69 59 82 100 50 75 18 100 31 59 0 38 37 38';
+
+  // var poly1 = this.add.polygon(700, 300, arrow, 0x0000ff, 0.2);
+  // objects.push(poly1)
+
+  // this.matter.add.gameObject(poly1, { shape: { type: 'fromVerts', verts: arrow, flagInternal: true } });
+
+  // poly1.setVelocity(0, 0);
+  // // poly1.setAngularVelocity(0.01);
+  // poly1.setBounce(1);
+  // poly1.setFriction(0, 0, 0);
+
+
+
+
+  var Bodies = Phaser.Physics.Matter.Matter.Bodies;
+
+  // var rectA = Bodies.rectangle(200, 200, 20, 20);
+  // var rectB = Bodies.rectangle(300, 300, 20, 20);
+
+  var rectA = Bodies.polygon(200, 200, 3, 10, {
+    isStatic: true
+  });
+  var rectB = Bodies.polygon(300, 300, 3, 10, {
+    isStatic: true
+  });
+
+  // rectB.setRotation(1)
+
+  compoundBody = Phaser.Physics.Matter.Matter.Body.create({
+      parts: [ rectA, rectB]
+  });
+  var block = this.matter.add.image(150, 0, 'block');
+  console.log('compoundBody')
+  console.log(compoundBody)
+  console.log('block')
+  console.log(block)
+  block.setAngularVelocity(1)
+
+  // this.tweens.add({
+  //   targets: block,
+  //   rotation: Phaser.Math.DegToRad(360),
+  //   duration: 800,
+  //   ease: 'Linear',
+  //   repeat: -1,
+  // });
+
+  block.setExistingBody(compoundBody);
+
+  compoundBody.gameObject.setBounce(5 )
+
+
+  
+
+  poly2 = this.add.polygon(100, 300, chevron, 0xff0000, 0.2);
+  objects.push(poly2)
+
+  this.matter.add.gameObject(poly2, { shape: { type: 'fromVerts', verts: chevron, flagInternal: true } });
+
+
+  poly2.setVelocity(1, 0);
+  poly2.setBounce(1);
+  poly2.setFriction(0, .001, 0);
+  
+
+  // var poly3 = this.add.polygon(600, 400, star, 0x00ff00, 0.2);
+  // objects.push(poly3)
+
+  // this.matter.add.gameObject(poly3, { shape: { type: 'fromVerts', verts: star, flagInternal: true } });
+
+  // poly3.setVelocity(4, -2);
+  // poly3.setBounce(1);
+  // poly3.setFriction(0, 0, 0);
+  // poly3.setFrictionAir(0.005);
+
+  // var cat1 = this.matter.world.nextCategory();
+
+  // poly2.setCollisionCategory(cat1);
+  // // poly3.setCollisionCategory(cat1);
+
+  // var cat2 = this.matter.world.nextCategory();
+
+  // poly1.setCollisionCategory(cat2);
+
+  // poly1.setCollidesWith([ cat1 ]);
+  // poly2.setCollidesWith([ cat2 ]);
+  // // poly3.setCollidesWith([ cat2 ]);
+
+  this.matter.world.on('collisionstart', function (event, bodyA, bodyB) {
+    event.pairs[0].separation = 0
+    console.log(event.pairs[0])
+  });
+}
+
+function update() {
+  compoundBody.gameObject.setAngularVelocity(.03)
+  compoundBody.gameObject.setPosition(200, 200)
+  if (Math.hypot(poly2.body.velocity.x, poly2.body.velocity.y) > 25) {
+    poly2.setFriction(0, 0.5, 0);
+  } else if (Math.hypot(poly2.body.velocity.x, poly2.body.velocity.y) > 15) {
+    poly2.setFriction(0, 0.1, 0);
+  } else if (Math.hypot(poly2.body.velocity.x, poly2.body.velocity.y) > 5) {
+    poly2.setFriction(0, 0.005, 0);
+  } else if (Math.hypot(poly2.body.velocity.x, poly2.body.velocity.y) > 2) {
+    poly2.setFriction(0, 0.001, 0);
+  } else {
+    poly2.setFriction(0, 0, 0);
+  }
+  console.log(Math.hypot(poly2.body.velocity.x, poly2.body.velocity.y))
+  _.each(objects, function(ob){
+    if(ob.x > 800) {
+      ob.x = 0
+    } else if (ob.x < 0) {
+      ob.x = 800
+    }
+    if(ob.y > 600) {
+      ob.y = 0
+    } else if (ob.y < 0) {
+      ob.y = 600
+    }
+  })
+}
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# asterogons
-Created for GeoJam 2021
+# plinko
+### version 0.1.4
+A simple HTML5 Canvas game made with Phaser and an HTML UI, click on balls to get score!
